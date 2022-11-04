0af1c647679fc7909e26102051258a30731742e2
Šimon Jurdič <Jurdic.Simon@stud.vassboskovice.cz>
2022-11-04 09:39:22 +0100

    karlosova prace

--- Had/MainWindow.xaml.cs
+++ Had/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Had
 {
@@ -20,9 +21,68 @@ namespace Had
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Circle> Snake = new List<Circle>();
+        private Circle food = new Circle();
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public void zacaljsemhrat()
+        {
+
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Tick += new EventHandler(hodinomaster);
+            dispatcherTimer.Start();
+
+        }
+        int seKundy = 0;
+        public void hodinomaster(object sender, EventArgs e )
+        {
+            seKundy++;
+            casovac.Content = seKundy;
+        }
+
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            zacaljsemhrat();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void UpdatePictureBoxGraphics(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RestarGame()
+        {
+
+        }
+
+        private void EateFood()
+        {
+
+        }
+
+        private void GameOver()
+        {
+
+        }
     }
+    
 }
