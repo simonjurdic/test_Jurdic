bd3a46b9e226497028d2d41c6334702e1ff973c6
Šimon Jurdič <Jurdic.Simon@stud.vassboskovice.cz>
2023-01-16 13:45:16 +0100

    Hotovo

---
+++ JurdicTest/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JurdicTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Soldier soldier1;
+        Soldier soldier2;
+
+        Random random = new Random();
+
+
+
+        public MainWindow()
+        {
+
+
+            InitializeComponent();
+            soldier1 = new Soldier("Petr");
+            soldier2 = new Soldier("Karel");
+
+            txtboxjedna.Text = soldier1.ToString();
+            txtboxdva.Text = soldier2.ToString();
+
+
+        }
+
+        public void Zobraz(Soldier soldier1, TextBox textBox)
+        {
+            textBox.Text = soldier1.Name + " ";
+            textBox.Text += "HP" + soldier1.MaxHp.ToString() + "\n";
+            textBox.Text += "Shield" + soldier1.MaxShield.ToString() + "\n";
+            textBox.Text += "Level" + soldier1.Level.ToString() + "\n";
+        }
+
+        private void Vojak2Utok2_Click(object sender, RoutedEventArgs e)
+        {
+            soldier2.GetHit(soldier1.Damage);
+            Zobraz1.Text = soldier2.ToString();
+        }
+
+        private void Vojak1Utok_Click(object sender, RoutedEventArgs e)
+        {
+            soldier1.GetHit(soldier2.Damage);
+            Zobraz2.Text = soldier1.ToString();
+        }
+
+
+        private void Vojak1Heal_Click(object sender, RoutedEventArgs e)
+        {
+            int randomHeal = random.Next(10, 15);
+            soldier1.Heal(randomHeal);
+            Zobraz1.Text = soldier1.ToString();
+        }
+
+        private void Vojak1Heal_Click2(object sender, RoutedEventArgs e)
+        {
+            int randomHeal = random.Next(10, 15);
+            soldier2.Heal(randomHeal);
+            Zobraz2.Text = soldier1.ToString();
+        }
+
+
+        private void LvlUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            soldier1.LevelUp();
+            Zobraz1.Text = soldier2.ToString();
+        }
+
+        private void LvlUpBtn_Click2(object sender, RoutedEventArgs e)
+        {
+            soldier2.LevelUp();
+            Zobraz2.Text = soldier2.ToString();
+        }
+    }
+
+}
---
+++ JurdicTest/Soldier.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JurdicTest
+{
+
+    public class Soldier
+    {
+        private int health;
+        private int maxHealth;
+        private int shield;
+        private int damage;
+        private int maxShield;
+        private int level;
+
+
+        public string Name { get; }
+        public int Hp { get => health; }
+        public int MaxHp { get => maxHealth; }
+        public int Damage { get => damage; }
+        public int MaxShield { get => maxShield; }
+        public int Level { get => level; }
+        public int MyProperty { get; set; } 
+
+        public Soldier(string name)
+        {
+            Name = name;
+            health = 50;
+            maxHealth = 100;
+            maxShield = 50;
+            shield = 25;
+            damage = 8;
+
+        }
+
+
+        public void GetHit(int damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                health += shield;
+                shield = 0;
+            }
+            health = Math.Max(0, health);
+
+        }
+        public void Heal(int heal)
+        {
+            health += heal;
+            health = Math.Min(maxHealth, health);
+        }
+        public void LevelUp()
+        {
+            damage += 2;
+            maxHealth += 5;
+            maxShield += 5;
+            level++;
+        }
+
+        public override string ToString()
+        {
+            string text = "Name: " + Name + "\n";
+            text += "Health:  " + Hp + "\n";
+            text += "Max Hp:  " + MaxHp + "\n";
+            text += "Damage:  " + damage + "\n";
+            text += "Shield:  " + shield + "\n";
+            text += "Max Shield:  " + maxShield + "\n";
+            text += "Level:  " + level + "\n";
+            return text;
+        }
+
+    }
+
+
+}
+
